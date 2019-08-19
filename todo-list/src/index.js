d3e0e1c7ea729b2fef41fb6035cfdf3805594564
Roka20012 <broslavsky.rostislaw@gmail.com>
2019-08-19 14:33:20 +0300

    make simple redux counter

---
+++ todo-list/src/index.js
@@ -0,0 +1,32 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Counter from "./Counter";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+
+const initialState = {
+    count: 42
+};
+function reducer(state = initialState, action) {
+    switch (action.type) {
+        case "INCREMENT":
+            return {
+                count: state.count + 1
+            };
+        case "DECREMENT":
+            return {
+                count: state.count - 1
+            };
+        default:
+            return state;
+    }
+}
+
+const store = createStore(reducer);
+
+ReactDOM.render(
+    <Provider store={store}>
+        <Counter />
+    </Provider>,
+    document.getElementById("root")
+);
